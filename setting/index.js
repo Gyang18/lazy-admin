4e8ed12d7cf2004001ed44e38b8f445faedb2767
yang <1763645020@qq.com>
2020-04-08 21:50:40 +0800

    完成tabbar对应页面的基础搭建

---
+++ setting/index.js
@@ -0,0 +1,18 @@
+/**
+ * 项目全局设置
+ * @param { string } title 项目名称、标题
+ * @param { string } logo 项目logo
+ * @param { boolean } auth 是否开启权限校验
+ * @param { string } apiDev 开发接口地址(不能为空)
+ * @param { string } apiTest 测试接口地址(不能为空)
+ * @param { string } api 生产接口地址(不能为空)
+ */
+
+module.exports = {
+  title: 'yang-shop',
+  logo: '',
+  auth: false,
+  apiDev: 'http://yapi.gyang.live/mock/11/app',
+  apiTest: 'http://api.test',
+  api: 'http://api',
+};
